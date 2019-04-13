11ff090389367c12502fd9e7d1973dbe64167dc7
Iago Bianquini <iagobianquini@gmail.com>
2019-04-13 12:08:03 -0300

    Estado e Cidade

---
+++ src/main/java/com/iagob/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,13 @@
+package com.iagob.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.iagob.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+	
+	
+}
